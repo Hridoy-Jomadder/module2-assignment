3f3c269092ccb1145f78ed9f4917e51c4f2a170c
Hridoy Jomadder <79590693+Hridoy-Jomadder@users.noreply.github.com>
2023-09-20 19:06:29 +0600

    Update task2.php

--- task2.php
+++ task2.php
@@ -1,1 +1,16 @@
 
+<?php
+echo " <h3>     Skip Multiples of 5  :      </h3> ";
+?>
+<?php
+for ($i = 1; $i <= 50; $i++) {
+    // Check if the current number is a multiple of 5
+    if ($i % 5 == 0) {
+        // Skip this iteration and continue to the next number
+        continue;
+    }
+    
+    // Print the current number
+    echo $i . " ";
+}
+?>
